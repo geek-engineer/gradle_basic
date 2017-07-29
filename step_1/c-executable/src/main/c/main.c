4ef207e9e8928e76176fe61be5e0b0385252b624
geek-engineer <kuotungtsai.me03g@g2.nctu.edu.tw>
2017-07-30 02:09:05 +0800

    Modify main.c

--- step_1/c-executable/src/main/c/main.c
+++ step_1/c-executable/src/main/c/main.c
@@ -2,7 +2,7 @@
 #include "greeting.h" 
 
 int main(int argc, char** argv) {
-    printf( "%s\n", GREETING_STRING);
+    // printf( "%s\n", GREETING_STRING);
     printf( "hello world!!");
 
     return 0;
